1279906ab744e8403351adfa3b518661ffbfe804
Aiaru Mukhamedyarova <aiaru.mukhamedyarova@gmail.com>
2021-01-07 10:47:18 +0900

    adding databse

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '0000',
-    database : 'face_brain_api'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
